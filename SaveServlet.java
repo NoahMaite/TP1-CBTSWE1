1479c39d2ccfbfd88396fb52e9a21902d856154a
Nayara Pereira <168305331+NoahMaite@users.noreply.github.com>
2026-04-01 18:22:16 -0300

    Implement SaveServlet for saving employee data

---
+++ SaveServlet.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/SaveServlet")
+public class SaveServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        Emp e = new Emp();
+        e.setName(name);
+        e.setPassword(password);
+        e.setEmail(email);
+        e.setCountry(country);
+
+        int status = EmpDao.save(e);
+
+        if (status > 0) {
+            response.getWriter().println("Registro salvo com sucesso!");
+        } else {
+            response.getWriter().println("Erro ao salvar!");
+        }
+    }
+}
